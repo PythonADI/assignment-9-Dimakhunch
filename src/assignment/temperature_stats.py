14d069c468dea6baee6f0cd14b3287effe9eb677
Dimakhunch <dima.khunch@gmail.com>
2024-11-12 20:49:41 +0400

    Update temperature_stats.py

--- src/assignment/temperature_stats.py
+++ src/assignment/temperature_stats.py
@@ -19,23 +19,24 @@ def get_temperature_stats(file_path):
     min_temp = +100
     min_temp_city = ""
     total_temp = 0
-    
+
     for city, temp in cities_temp.items():
         temp = float(temp)
         total_temp += temp
-        
+
         if temp > max_temp:
             max_temp = float(temp)
             max_temp_city = city
-        
+
         if temp < min_temp:
             min_temp = temp
             min_temp_city = city
 
     avr_temp = total_temp / len(cities_temp)
     temp_range = max_temp - min_temp
-    
-    temperature_stats = {"hottest_city": max_temp_city,
+
+    temperature_stats = {
+                        "hottest_city": max_temp_city,
                         "coldest_city": min_temp_city,
                         "average_temperature": avr_temp,
                         "temperature_range": temp_range,
