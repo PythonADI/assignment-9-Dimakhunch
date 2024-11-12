c3d97a8808a1d57351d59842dd9d39bed6c505f0
Dimakhunch <dima.khunch@gmail.com>
2024-11-12 20:42:30 +0400

    Update temperature_stats_v2.py

--- src/assignment/temperature_stats_v2.py
+++ src/assignment/temperature_stats_v2.py
@@ -10,3 +10,46 @@ def get_temperature_stats_v2(file_path):
     - temperature_range: difference between highest and lowest temperature
     """
     # wrtie your code here
+cities_temp = {}
+
+    with open(file_path, 'r') as f:
+        for line in f:
+            city, temp = line.strip().split(',')
+            temp = float(temp)
+            
+            if city not in cities_temp:
+                cities_temp[city] = []
+            cities_temp[city].append(temp)
+
+    max_temp = -100  
+    max_temp_city = ""
+    min_temp = 100   
+    min_temp_city = ""
+    total_temp = 0
+    total_count = 0
+    
+    for city, temps in cities_temp.items():
+        avg_city_temp = sum(temps) / len(temps)  
+        total_temp += avg_city_temp
+        total_count += 1
+        
+        if avg_city_temp > max_temp:
+            max_temp = avg_city_temp
+            max_temp_city = city
+        
+        if avg_city_temp < min_temp:
+            min_temp = avg_city_temp
+            min_temp_city = city
+
+    average_temperature = total_temp / total_count
+    
+    temperature_range = max_temp - min_temp
+    
+    temperature_stats_v2 = {
+        "hottest_city": max_temp_city,
+        "coldest_city": min_temp_city,
+        "average_temperature": average_temperature,
+        "temperature_range": temperature_range
+    }
+    
+    return temperature_stats_v2
